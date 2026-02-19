47dd43dcf5473930c7581b417d7b41f310e39b30
Sailfish <nuoyan_cfan@163.com>
2026-02-20 02:51:52 +0800

    chore(lint): simplify feishu-adapter empty catch comment per review

--- electron/services/im/feishu-adapter.ts
+++ electron/services/im/feishu-adapter.ts
@@ -90,7 +90,7 @@ export class FeishuAdapter implements IMAdapter {
     } catch (err) {
       this.updateConnected(false)
       if (this.wsClient) {
-        try { this.wsClient.close({ force: true }) } catch { /* close may throw, ignore */ }
+        try { this.wsClient.close({ force: true }) } catch { /* ignore */ }
         this.wsClient = null
       }
       this.client = null
